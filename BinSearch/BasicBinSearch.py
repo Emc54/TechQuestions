b69c4690a6e8faaa307f9a0535a63c1ae435af23
Emc54 <35582806+Emc54@users.noreply.github.com>
2024-07-02 23:36:03 +0100

    Simplest binary search

--- BinSearch/BasicBinSearch.py
+++ BinSearch/BasicBinSearch.py
@@ -7,4 +7,23 @@ inserted in order.
 """
 
 
+sorted_arr = [1,2,4,5,7,8,9]
 
+def indexFinder(arr: list, target: int)-> int:
+    
+    left, right = 0, len(arr) 
+
+    while left < right:
+
+        mid = (right + left) // 2
+
+        if arr[mid] >= target:
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
+
+for i in range(10):
+    print(f"The number {i} goes at {indexFinder(sorted_arr,i)} in the array")
+         
