ae53622bfadac40180c2553e72f63ef11dec7b7b
Emc54 <emilioschristou@hotmail.com>
2024-07-07 19:58:07 +0100

    Introduces a first attempt at the ugly3 problem

---
+++ BinSearch/ugly3.py
@@ -0,0 +1,11 @@
+"""
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive integers which are divisible by a or b or c.
+
+Input: n = 3, a = 2, b = 3, c = 5
+Output: 4
+Explanation: The ugly numbers are 2, 3, 4, 5, 6, 8, 9, 10... The 3rd is 4.
+"""
+
+
